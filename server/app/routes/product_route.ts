b6aee0dc02904f30bbd12a478219ac70af814615
Kaushal <kaushalk.dev@gmail.com>
2024-08-20 22:54:55 +0530

    WIP : Product list creation.

---
+++ server/app/routes/product_route.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import ProductModel from '../models/product_model'; '../models/product_model';
+
+const router = express.Router();
+
+// GET /products
+router.get('/products', (req: Request, res: Response) => {
+     
+ 
+
+ 
+});
+
+export default router;
